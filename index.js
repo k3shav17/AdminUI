bba3bf8b344af2e5200a7d0c6034e4ca1e3ca55a
k3shav17 <keshavrao0489@gmail.com>
2022-07-30 22:36:19 +0530

    going to work on it again

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const searchBar = document.getElementById("searchBar");
 const memberList = document.getElementById("dataTable");
-const nameId = document.getElementById("name");
 let members = [];
 
 searchBar.addEventListener("keyup", (event) => {
-  console.log(event);
   const searchString = event.target.value.toLowerCase();
   const filteredMembers = members.filter((character) => {
     return (
@@ -33,18 +31,20 @@ const displayMembers = (membersData) => {
     .map((membersData) => {
       return `<tr>
           <td><input type="checkbox"></td>
-          <td class="data">${membersData.name}</td>
-          <td class="data">${membersData.email}</td>
-          <td class="data">${membersData.role}</td>
+          <td>${membersData.name}</td>
+          <td>${membersData.email}</td>
+          <td>${membersData.role}</td>
 
           <td>
-          <button class="btn btn-secondary" onclick="editMember(event)" id="${membersData.id}" value="edit">
+          <button class="btn btn-secondary" onclick="editMember()" id="${membersData.id}">
           <i class="fa-solid fa-pen-to-square"></i>
           </button>
           </td>
 
           <td>
-          <button onclick="deleteMember(event)" id="${membersData.id}" class="btn btn-danger"><i class="fa-solid fa-trash"></i></button>
+          <button onclick="deleteMember(event)" id="${membersData.id}" class="btn btn-danger">
+          <i class="fa-solid fa-trash"></i>
+          </button>
           </td>
 
           </tr>`;
@@ -54,13 +54,9 @@ const displayMembers = (membersData) => {
   memberList.innerHTML = htmlString;
 };
 
-function editMember(event) {
-  $(this).parent().siblings('td.data').each(function () {
-    let content = $(this).html();
-    $(this).html('<input value="' + content + '"/>') ;
-  })
+function editMember() {
 
-  console.log(event.target);
+  
 }
 
 function deleteMember(event) {
